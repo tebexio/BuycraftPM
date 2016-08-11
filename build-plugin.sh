c559b528f2b231bba39cda482e27b95c5ac71a68
Tux <write@imaginarycode.com>
2016-08-11 04:46:41 -0400

    Use ini_get() instead.

--- build-plugin.sh
+++ build-plugin.sh
@@ -4,7 +4,7 @@
 rm -f BuycraftPM.phar
 
 # Check if phar.readonly is Off
-if [ `php -r 'print get_cfg_var("phar.readonly") ? "false" : "true";'` == false ]; then
+if [ `php -r 'print ini_get("phar.readonly") ? "false" : "true";'` == false ]; then
     echo "PHAR creation is not enabled in your php.ini. Please set phar.readonly = Off and try again."
     exit 1
 fi
